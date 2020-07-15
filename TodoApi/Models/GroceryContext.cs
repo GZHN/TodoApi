8dda3fbcf4a43a6470da8e0f150996e8065b1d58
Oguzhan <GZHN@live.com>
2020-07-15 13:12:53 +0200

    Initial commit

---
+++ TodoApi/Models/GroceryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class GroceryContext : DbContext
+    {
+        public GroceryContext(DbContextOptions<GroceryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GroceryItem> GroceryItems { get; set; }
+    }
+}
---
+++ TodoApi/Models/GroceryItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class GroceryItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Supermarket { get; set; }
+        public int Quantity { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime Added { get; set; }
+        public GroceryItem()
+        {
+            Added = DateTime.Now;
+        }
+    }
+}
